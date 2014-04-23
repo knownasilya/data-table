60237f550dc7783aa729d731eca238a081d4ce6c
Ilya Radchenko <ilya@burstcreations.com>
2014-04-23 14:22:56 -0400

    data dsipalyed and transformed

--- dist/data-table.template.js
+++ dist/data-table.template.js
@@ -1,9 +1,44 @@
 Ember.TEMPLATES["components/data-table"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
 this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
-  var buffer = '';
+  var buffer = '', stack1, self=this;
 
+function program1(depth0,data) {
+  
+  var buffer = '', stack1;
+  data.buffer.push("\n      <th>");
+  stack1 = helpers._triageMustache.call(depth0, "name", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("</th>\n    ");
+  return buffer;
+  }
 
+function program3(depth0,data) {
+  
+  var buffer = '', stack1;
+  data.buffer.push("\n    <tr>\n      ");
+  stack1 = helpers.each.call(depth0, "", {hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(4, program4, data),contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n    </tr>\n  ");
+  return buffer;
+  }
+function program4(depth0,data) {
+  
+  var buffer = '', stack1;
+  data.buffer.push("\n        <td>");
+  stack1 = helpers._triageMustache.call(depth0, "", {hash:{},hashTypes:{},hashContexts:{},contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("</td>\n      ");
+  return buffer;
+  }
+
+  data.buffer.push("<thead>\n  <tr>\n    ");
+  stack1 = helpers.each.call(depth0, "columns", {hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n  </tr>\n</thead>\n<tbody>\n  ");
+  stack1 = helpers.each.call(depth0, "data", {hash:{},hashTypes:{},hashContexts:{},inverse:self.noop,fn:self.program(3, program3, data),contexts:[depth0],types:["ID"],data:data});
+  if(stack1 || stack1 === 0) { data.buffer.push(stack1); }
+  data.buffer.push("\n</tbody>\n");
   return buffer;
   
 });
--- src/component.js
+++ src/component.js
@@ -1,2 +1,52 @@
 var DataTableComponent = Ember.Component.extend({
+  tagName: 'table',
+  classNames: ['table', 'table-responsive', 'table-hover', 'table-condensed'],
+  columns: [
+    { name: 'Name', attributes: ['name'] },
+    { name: 'State', attributes: ['us_state', 'state'] }
+  ],
+  data: function () {
+    var datasets = this.get('datasets');
+    var columns = this.get('columns');
+    var self = this;
+
+    if (!Ember.isArray(datasets)) {
+      throw new Error('Datasets input must be an array.');
+    }
+
+    return datasets.reduce(function (previous, current) {
+      if (!Ember.isArray(current)) {
+        throw new Error('Dataset must be an array.');
+      }
+
+      var mapped = current.map(function (item) {
+        return self.columnAttributeMap(columns, item);
+      });
+
+      return previous.concat(mapped);
+    }, []);
+  }.property('datasets'),
+
+  columnAttributeMap: function (columns, row) {
+    if (!row) {
+      return;
+    }
+
+    var result = [],
+      col = 0,
+      prop, columnIndex;
+
+    for (prop in row) {
+      if (row.hasOwnProperty(prop)) {
+        for(; col < columns.length; col++) {
+          if (columns[col].attributes.contains(prop)) {
+            result.splice(col, 0, row[prop]);
+          }
+        }
+        col = 0;
+      }
+    }
+
+    return result;
+  }
 });
